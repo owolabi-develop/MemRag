dc7e7e8efab533b39c6ce13d00c39a648b98573e
owolabi-develop <owolabidevelop84@gmail.com>
2026-06-27 18:08:59 +0100

    update chunking from recursive to document level chunking

--- Backend/src/Ingestion/loaders.py
+++ Backend/src/Ingestion/loaders.py
@@ -1,44 +1,32 @@
-from pypdf import PdfReader
 from pprint import pprint
-from src.chuncking.chunker import recursive_chunking
 from datetime import datetime
 import json
+from pathlib import Path
 from src.embeddings.embedder import hug_embedding
 from src.connection.connections import get_db_pool
+import pymupdf4llm
+from langchain_text_splitters import MarkdownTextSplitter
 
 async def load_document(file, department:str, tenant_id:str, table_name : str="SEMANTIC_MEMORY"):
-    reader = PdfReader(file)
-    meta = reader.metadata
-    pages = []
-    for page_num,page in enumerate(reader.pages):
-        text = page.extract_text()
-        pages.append(
-            {"page": page_num +1,"text":text})
-
-    all_chunks = []
-    ## chunk documents and get metadata
-    print("chunking documents ...")
-    for page in pages:
-        
-        chunk = await recursive_chunking(page['text'])
-        
-        
-        for chunk_idx, chunk in enumerate(chunk):
-            all_chunks.append(
-                {
-                    "text":chunk,
-                    "metadata":{
-                        "source":"report.pdf",
-                        "page":page["page"],
-                        "chunk":chunk_idx,
-                        "title":meta.get("/Title"),
-                        "author":meta.get("/Author"),
-                        "created_at":datetime.now().isoformat()
-                    }
-                }
-            )
+    source = Path(file).name
+    print(f"loading document: {source} for department: {department} and tenant_id: {tenant_id}")
+    md_text = pymupdf4llm.to_markdown(file,page_chunks=True)
+    splitter = MarkdownTextSplitter(chunk_size=1000, chunk_overlap=0)
     
-    ## ingest chunk documents and metadata to vector db
+    # get page data and metadata
+    pages = [{"text": page['text'], "metadata":page['metadata']} for page in md_text]
+    
+    all_chunks = []
+    for page in pages:
+        chunks = splitter.split_text(page['text'])
+        for idx, chunk in enumerate(chunks):
+            all_chunks.append(
+                {"text": chunk, 
+                 "metadata":{"source":source, "department": department, "tenant_id": tenant_id, "page_number": page['metadata'].get('page_number'),"title": page['metadata'].get('title'), "chunk_index": idx, "timestamp": datetime.now().isoformat()}
+            })
+    pprint(all_chunks,indent=4)
+    
+    # ingest chunk documents and metadata to vector db
     pool = await get_db_pool()
     print("ingesting documents...")
     for _chunk in all_chunks:
