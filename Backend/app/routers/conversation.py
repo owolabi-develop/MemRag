b93a292eddb782db884543c68f7052039bcab344
owolabi-develop <owolabidevelop84@gmail.com>
2026-06-29 22:32:12 +0100

    add conversation history

--- Backend/app/routers/conversation.py
+++ Backend/app/routers/conversation.py
@@ -4,6 +4,7 @@ from app.dependencies import sessionCreator
 from app.models import User, Conversation,ConversationPublic
 from typing import Annotated
 from app.security import get_current_active_user
+from sqlmodel import select
 
 router = APIRouter(
     prefix='/conversations',
@@ -11,9 +12,13 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=ConversationPublic)
+@router.get("/", response_model=list[ConversationPublic])
 async def get_conversation(current_user: Annotated[User, Depends(get_current_active_user)],session:sessionCreator):
      user_id = current_user.id
+     existing_conversation = await session.exec(select(Conversation).where(Conversation.owner_id == user_id).order_by(Conversation.con_timestamp))
+     if not existing_conversation:
+         return []
+     return existing_conversation
 
 
 
--- Backend/app/routers/users.py
+++ Backend/app/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi import Depends, HTTPException
 from app.dependencies import sessionCreator
-from app.models import User,UserCreate,UserPublic
+from app.models import User,UserCreate,UserPublic,Tenant
 from app.security import get_password_hash,get_current_active_user
 from sqlmodel import select
 from typing import Annotated
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/users",
                    tags=["users"],
                    responses={404: {"description": "Not found"}},)
 
-@router.post("/create",response_model=UserPublic)
+@router.post("/register",response_model=UserPublic)
 async def create_user(user: UserCreate, session:sessionCreator):
     existing_user = await session.exec(select(User).where(User.email == user.email))
     if existing_user.first():
@@ -21,6 +21,30 @@ async def create_user(user: UserCreate, session:sessionCreator):
     session.add(user_obj)
     await session.commit()
     await session.refresh(user_obj)
+    return user_obj
+
+@router.post("/add/{tenant_id}",response_model=UserPublic)
+async def add_user(user: UserCreate, session:sessionCreator,current_user: Annotated[User,Depends(get_current_active_user)],tenant_id:str):
+    existing_user = await session.exec(select(User).where(User.email == user.email))
+    
+    if existing_user.first():
+        raise HTTPException(status_code=400, detail="Username already registered")
+    
+    existing_tenant_org = await session.get(Tenant,tenant_id)
+    if existing_tenant_org is None:
+        raise HTTPException(status_code=404, detail="Organization not found")
+        
+    user_obj = User.model_validate(user,update={"hashed_password": get_password_hash(user.password)})
+    
+    user_obj.tenant_id = existing_tenant_org.id
+    
+    session.add(user_obj)
+    await session.commit()
+    await session.refresh(user_obj)
+    
+    ## get tenant org
+    
+    
     return user_obj
 
 @router.get("/", response_model=UserPublic)
